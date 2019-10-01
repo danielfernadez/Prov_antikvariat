3c096baf673f36e30f91d065130a98700c9211aa
danielfernadez <55138329+danielfernadez@users.noreply.github.com>
2019-10-01 13:39:54 +0200

    inlämning

---
+++ Prov_Antikvariatägarens dotter/Prov_Antikvariatägarens dotter/book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_Antikvariatägarens_dotter
+{
+    class Book
+    {
+        public int price;
+        private string name;
+        private string rarity;
+        private string category;
+        private int actualValve;
+        private bool cursed;
+        private Random generator;
+
+        public Book()
+        {
+           
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("");
+        }
+        public int Evaluate();
+        
+
+    }
+
+}
